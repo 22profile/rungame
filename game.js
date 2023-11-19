611e43c53777208e26407144aa474b1490db2ce8
22profile <111512185+22profile@users.noreply.github.com>
2023-11-19 14:32:26 +0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,255 @@
+// game.js
+
+let playerX = 50;
+let playerY = 50;
+const playerSpeed = 5;
+let gameStarted = false;
+let startTime = 0;
+let elapsedTime = 0;
+let recordTime = getRecordFromLocalStorage();
+let coinCount = 0;
+let coins = [];
+let zombies = [];
+const zombieSpeed = 2;
+const zombieSize = 50;
+let spawnInterval = 150; 
+let coinIndex = 0;
+
+function createZombie() {
+  let side = Math.floor(random(4));
+  let zombie = {
+    x: 0,
+    y: 0,
+    move: function () {
+      if (playerX < this.x) {
+        this.x -= zombieSpeed;
+      } else {
+        this.x += zombieSpeed;
+      }
+      if (playerY < this.y) {
+        this.y -= zombieSpeed;
+      } else {
+        this.y += zombieSpeed;
+      }
+    },
+    display: function () {
+      fill(0, 255, 0);
+      rect(this.x, this.y, zombieSize, zombieSize);
+    },
+    initializePosition: function () {
+      switch (side) {
+        case 0:
+          this.x = random(width);
+          this.y = -zombieSize;
+          break;
+        case 1:
+          this.x = random(width);
+          this.y = height;
+          break;
+        case 2:
+          this.x = -zombieSize;
+          this.y = random(height);
+          break;
+        case 3:
+          this.x = width;
+          this.y = random(height);
+          break;
+      }
+    },
+  };
+  zombie.initializePosition();
+  return zombie;
+}
+
+function createCoin() {
+  let coin = {
+    x: random(width),
+    y: random(height),
+    size: 20,
+    collected: false,
+    display: function () {
+      if (!this.collected) {
+        fill(255, 255, 0);
+        ellipse(this.x, this.y, this.size, this.size);
+      }
+    },
+    checkCollision: function () {
+  if (!this.collected) {
+    if (
+      playerX < this.x + this.size &&
+      playerX + 50 > this.x &&
+      playerY < this.y + this.size &&
+      playerY + 50 > this.y
+    ) {
+      this.collected = true;
+      coinCount++;
+      coinIndex++; 
+      if (coinIndex < coins.length) {
+        coins[coinIndex] = createCoin(); 
+      }
+    }
+  }
+},
+  };
+  return coin;
+}
+
+function setup() {
+  createCanvas(windowWidth * 0.99, windowHeight * 0.975);
+  window.addEventListener("focus", windowFocus);
+  window.addEventListener("blur", windowBlur);
+  startTime = getCurrentTime() - elapsedTime;
+  spawnCoins();
+}
+
+function windowFocus() {
+  if (gameStarted) {
+    loop();
+    startTime = getCurrentTime() - elapsedTime;
+  }
+}
+
+function windowBlur() {
+  if (gameStarted) {
+    noLoop();
+    elapsedTime = getCurrentTime() - startTime;
+  }
+}
+
+function spawnCoins() {
+  let coinCountLimit = floor(recordTime) - 1; 
+  for (let i = 0; i < coinCountLimit; i++) {
+    coins.push(createCoin());
+  }
+}
+
+function draw() {
+  background(220);
+
+  if (!gameStarted) {
+    fill(0);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("Нажмите Enter, чтобы начать", width / 2, height / 2);
+
+    if (keyIsDown(ENTER)) {
+      gameStarted = true;
+      startTime = getCurrentTime();
+      saveStartTimeToLocalStorage(startTime);
+      zombies = [];
+      loop();
+    }
+  } else {
+    if (frameCount % spawnInterval === 0) {
+      zombies.push(createZombie());
+    }
+
+    for (let i = zombies.length - 1; i >= 0; i--) {
+      zombies[i].move();
+      zombies[i].display();
+
+      if (
+        playerX < zombies[i].x + zombieSize &&
+        playerX + 50 > zombies[i].x &&
+        playerY < zombies[i].y + zombieSize &&
+        playerY + 50 > zombies[i].y
+      ) {
+        gameStarted = false;
+        elapsedTime = getCurrentTime() - startTime;
+        if (elapsedTime > recordTime) {
+          recordTime = elapsedTime;
+          saveRecordToLocalStorage();
+        }
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        fill(0, 0, 0);
+        text(
+          `Ваш результат: ${nf(elapsedTime / 1000, 0, 2)} секунд`,
+          width / 2,
+          height / 2 + 50
+        );
+        text(
+          `Рекордное время: ${nf(recordTime / 1000, 0, 2)} секунд`,
+          width / 2,
+          height / 2 + 80
+        );
+        text(`Подобрано монет: ${coinCount}`, width / 2, height / 2 + 110);
+		text("Для рестарта нажмите Enter", width / 2, height / 2 - 110);
+		elapsedTime = 0;
+        noLoop();
+      }
+    }
+
+    for (let i = 0; i <= coinIndex; i++) {
+      coins[i].display();
+      coins[i].checkCollision();
+    }
+
+    let prevPlayerX = playerX;
+    let prevPlayerY = playerY;
+
+    if (keyIsDown(87) && playerY > 0) {
+      playerY -= playerSpeed;
+    }
+
+    if (keyIsDown(65) && playerX > 0) {
+      playerX -= playerSpeed;
+    }
+
+    if (keyIsDown(83) && playerY < height - 50) {
+      playerY += playerSpeed;
+    }
+
+    if (keyIsDown(68) && playerX < width - 50) {
+      playerX += playerSpeed;
+    }
+
+    fill(255, 0, 0);
+    rect(playerX, playerY, 50, 50);
+
+    textSize(16);
+    textAlign(LEFT, TOP);
+    fill(0);
+    text(
+      `Прошедшее время: ${nf((getCurrentTime() - startTime) / 1000, 0, 2)} секунд`,
+      10,
+      10
+    );
+    text(`Рекордное время: ${nf(recordTime / 1000, 0, 2)} секунд`, 10, 30);
+    text(`Подобрано монет: ${coinCount}`, 10, 50);
+  }
+}
+
+function saveRecordToLocalStorage() {
+  localStorage.setItem("record", recordTime);
+}
+
+function getRecordFromLocalStorage() {
+  return parseFloat(localStorage.getItem("record")) || 0;
+}
+
+function saveStartTimeToLocalStorage(time) {
+  localStorage.setItem("startTime", time);
+}
+
+function getCurrentTime() {
+  return millis();
+}
+
+function getElapsedTimeFromLocalStorage() {
+  return getCurrentTime() - parseFloat(localStorage.getItem("startTime")) || 0;
+}
+
+function keyPressed() {
+  if (keyCode === ENTER && !gameStarted) {
+    gameStarted = true;
+    startTime = getCurrentTime() - elapsedTime;
+    saveStartTimeToLocalStorage(startTime);
+    zombies = [];
+    coins = [];
+    coinCount = 0;
+    coinIndex = 0;
+    spawnCoins();
+    loop();
+  }
+}
